f76e8895660991865f5f0bb500f7408282c2c4ad
Alex02Mx <alexloudog34@gmail.com>
2023-03-01 21:57:45 -0600

    Second commit

--- jobs.php
+++ jobs.php
@@ -7,7 +7,7 @@ function printJob($job){
             <h5> {$job->title} </h5>
             <p> {$job->description} </p>
             <p><strong>  {$job->getDuration()} </strong></p>
-            <strong>Achievements:</strong>
+            <strong> Achievements: </strong>
             <ul>
                 <li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>
                 <li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>
